cfc221c089e3f8d20b2d1e47009d3e74d2450644
vamsi nalla <raghuvamsinalla@gmail.com>
2016-06-30 21:42:06 +0530

    css

--- form.js
+++ form.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + "/" +"form.html"));
+    res.sendFile(path.join(__dirname + "/" +"form.html" +"form.css"));
 });
 
 
